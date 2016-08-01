888535883e6268d82fd1dd24596a5f629c381581
Matthew Lemanski <lemanski@ccs.neu.edu>
2016-07-31 23:09:59 -0400

    initial API endpoints for retrieving a profile and performing a search

---
+++ SearchOutlets/Controllers/ProfileController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SearchOutlets.Controllers
+{
+    /// <summary>
+    /// Retrieve a profile by given ID
+    /// </summary>
+    public class ProfileController : ApiController
+    {
+
+    }
+}
---
+++ SearchOutlets/Controllers/SearchController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SearchOutlets.Controllers
+{
+    /// <summary>
+    /// Execute a search based on the items given in the text field
+    /// </summary>
+    public class SearchController : ApiController
+    {
+
+    }
+}
