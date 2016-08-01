6c0a4346f5141e2214691641426f610c71ea7108
Matthew Lemanski <lemanski@ccs.neu.edu>
2016-07-31 23:14:00 -0400

    Creating an in-memory datastore for profile data from Contacts.json

---
+++ SearchOutlets/Datastores/ProfileData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections;
+
+namespace SearchOutlets.Datastores
+{
+    public class ProfileData
+    {
+        private Dictionary<int, Models.Contact> Profiles { get; set; }
+
+        public void Load()
+        {
+            List<Models.Contact> contacts;
+
+            using (StreamReader sr = new StreamReader("Contacts.json"))
+            {
+                string json = sr.ReadToEnd();
+                contacts = JsonConvert.DeserializeObject<List<Models.Contact>>(json);
+            }
+
+            foreach (Models.Contact contact in contacts)
+            {
+                Profiles.Add(contact.Id(), contact);
+            }
+        }
+    }
+}
