1f4ee041c3ad87a6ccf44f2579a9693d2c979ea4
Matthew Lemanski <lemanski@ccs.neu.edu>
2016-07-31 23:09:17 -0400

    adding initial models - internal representations of Contact and Outlet

---
+++ SearchOutlets/Models/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SearchOutlets.Models
+{
+    /// <summary>
+    /// Internal representation of an item from the Contacts.json file
+    /// </summary>
+    public class Contact
+    {
+        private int Id { get; set; }
+        private int OutletId { get; set; } // only 1 Outlet
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+        private string Title { get; set; }
+        private string Profile { get; set; }
+    }
+}
---
+++ SearchOutlets/Models/Outlet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SearchOutlets.Models
+{
+    /// <summary>
+    /// Internal representation of an item from the Outlets.json file
+    /// </summary>
+    public class Outlet
+    {
+        private int Id { get; set; }
+        private string Name { get; set; }
+    }
+}
