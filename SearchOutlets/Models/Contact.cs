df50058778ff2529083cd5d24397551ba538a93a
Matthew Lemanski <lemanski@ccs.neu.edu>
2016-08-02 02:22:47 -0400

    adding Contact class for GUI/Index interaction

---
+++ SearchOutlets/Models/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SearchOutlets.Models
+{
+    /// <summary>
+    /// A contact's profile data that will displayed in the GUI. Different
+    /// from Contact, Outlet, and JsonData in that those are representations
+    /// of the associated JSON objects.
+    /// 
+    /// This class will be used for interacting with the index.
+    /// </summary>
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Outlet { get; set; }
+        public string Profile { get; set; }
+    }
+}
