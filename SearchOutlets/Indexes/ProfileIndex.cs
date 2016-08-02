a2b9253614198b53fc3f04955e61a3cc0207c72a
Matthew Lemanski <lemanski@ccs.neu.edu>
2016-08-01 23:26:55 -0400

    adding Lucene index for profile data

---
+++ SearchOutlets/Indexes/ProfileIndex.cs
@@ -0,0 +1,43 @@
+﻿using Lucene.Net.Store;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SearchOutlets.Indexes
+{
+    /// <summary>
+    /// This class controls all access to the profile index.
+    /// 
+    /// This exercise contains a pretty small amount of text data, so it
+    /// should be fine to just keep in memory. If there was more data we
+    /// could use the file system-based index.
+    /// </summary>
+    public class ProfileIndex
+    {
+        private static ProfileIndex instance;
+
+        // private constructor to support the singleton model
+        private ProfileIndex() { }
+
+        // return the single instance of ProfileIndex, initializing if necessary
+        public static ProfileIndex Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ProfileIndex();
+                }
+
+                return instance;
+            }
+        }
+
+
+
+        // the index storing all profile data
+        private Directory index = new RAMDirectory();
+
+    }
+}
