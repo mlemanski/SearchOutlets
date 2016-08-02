5f995dcf4d83b31db874bd248b3739b028c35b35
Matthew Lemanski <lemanski@ccs.neu.edu>
2016-08-01 22:35:45 -0400

    handle the 'all profiles' case

--- SearchOutlets/Controllers/ProfileController.cs
+++ SearchOutlets/Controllers/ProfileController.cs
@@ -12,20 +12,32 @@ namespace SearchOutlets.Controllers
     /// </summary>
     public class ProfileController : ApiController
     {
+        private const int ALL_PROFILES = -1;
+
         /// <summary>
         /// Retrieve the Contact matching the given identifier, if any such Contact exists
         /// </summary>
         /// <param name="id"></param>
         /// <returns></returns>
         [HttpGet]
-        public IHttpActionResult Profile(int id)
+        public IHttpActionResult Profile(int id = ALL_PROFILES)
         {
-            Models.Contact profile = null;
-            Datastores.ProfileDatastore.Instance.ProfileData.TryGetValue(id, out profile);
-
-            if (profile != null)
+            // get all profiles
+            if (id == ALL_PROFILES)
             {
-                return Ok(profile);
+                List<Models.Contact> allProfiles = new List<Models.Contact>(Datastores.ProfileDatastore.Instance.ProfileData.Values);
+                return Ok(allProfiles);
+            }
+            // get the profile for the specified contact ID
+            else
+            {
+                Models.Contact profile = null;
+                Datastores.ProfileDatastore.Instance.ProfileData.TryGetValue(id, out profile);
+
+                if (profile != null)
+                {
+                    return Ok(profile);
+                }
             }
 
             return NotFound();
