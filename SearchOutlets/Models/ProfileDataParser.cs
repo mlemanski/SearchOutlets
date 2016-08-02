d1e7ad5c4b3fd79cf79b05e21ae01ef961f150f5
Matthew Lemanski <lemanski@ccs.neu.edu>
2016-08-02 01:47:47 -0400

    using generics for parsing

---
+++ SearchOutlets/Models/ProfileDataParser.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchOutlets.Models
+{
+    /// <summary>
+    /// A common tag for Contacts and Outlets
+    /// </summary>
+    class ProfileDataParser<T> where T : ProfileData
+    {
+        private const string JSON_PATH = "C:\\Users\\Matt\\Documents\\Visual Studio 2015\\Projects\\SearchOutlets\\SearchOutlets\\Datastores\\";
+        private const string CONTACTS = JSON_PATH + "Contacts.json";
+        private const string OUTLETS = JSON_PATH + "Outlets.json";
+
+
+        /// <summary>
+        /// Load the JSON file into a List of objects
+        /// </summary>
+        /// <returns></returns>
+        public List<T> LoadProfileData()
+        {
+            List<T> data;
+
+            string json_path = typeof(T) == typeof(Contact) ? CONTACTS : OUTLETS;
+
+            using (StreamReader sr = new StreamReader(json_path))
+            {
+                string json = sr.ReadToEnd();
+                data = JsonConvert.DeserializeObject<List<T>>(json);
+            }
+
+            return data;
+        }
+
+
+        /// <summary>
+        /// Load the JSON file into a dictionary of ID-to-object mappings
+        /// </summary>
+        /// <returns></returns>
+        public Dictionary<int, T> LoadProfileDataMap()
+        {
+            Dictionary<int, T> dataMap = new Dictionary<int, T>();
+            foreach (T item in LoadProfileData())
+            {
+                dataMap.Add(item.Id, item);
+            }
+
+            return dataMap;
+        }
+    }
+}
