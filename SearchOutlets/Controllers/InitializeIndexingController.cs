1a282c143b60d4f2efd4cde354761048ca66b14e
Matthew Lemanski <lemanski@ccs.neu.edu>
2016-08-03 04:38:35 -0400

    added InitializeIndexingController to force the index to initialize as soon as the page loads, rather than when the first user request is received. Saves 1-10 seconds on the first request.

---
+++ SearchOutlets/Controllers/InitializeIndexingController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SearchOutlets.Controllers
+{
+    public class InitializeIndexingController : ApiController
+    {
+        /// <summary>
+        /// Indexing takes about 10 seconds to do, which makes that initial 
+        /// request take a misleadingly long amount of time. So, start 
+        /// initializing as soon as the web page is loaded.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IHttpActionResult InitializeIndexing()
+        {
+            // making a reference to the singleton instance will invoke initialization
+            if (Datastores.ProfileIndex.Instance != null)
+                System.Diagnostics.Debug.WriteLine("Pre-initializing index");
+
+            return Ok();
+        }
+    }
+}
